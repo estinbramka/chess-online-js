a7264ac1e19e441b9cacda616c38a7996dacfd1a
estinbramka <estin1996@gmail.com>
2023-01-23 18:03:32 +0200

    rendered board

--- src/pages/Game/index.jsx
+++ src/pages/Game/index.jsx
@@ -1,12 +1,24 @@
-import React, { useState, useRef } from 'react';
-import Chess from 'Chess.js';
+import React, { useState, useRef, useEffect } from 'react';
+//import Chess from 'chess.js';
+import { Chess } from 'chess.js'
+import { createBoard } from '../../functions';
+import Board from '../../components/board';
 
 const FEN = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1';
 const Game = () => {
     const [fen, setFen] = useState(FEN);
     const { current: chess } = useRef(new Chess(fen));
+    const [board, setBoard] = useState(createBoard(fen));
 
-    return <div></div>;
+    useEffect(() => {
+        setBoard(createBoard(fen));
+    }, [fen]);
+
+    return (
+        <div className="game">
+            <Board cells={board} />
+        </div>
+    );
 };
 
 export default Game;
