d354a960c7cc42d0db00dac2ba67a785284261c2
estinbramka <estin1996@gmail.com>
2023-02-08 00:54:40 +0200

    added drag and drop moves

--- src/components/piece/index.jsx
+++ src/components/piece/index.jsx
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import PropTypes from 'prop-types';
 import './piece-styles.css';
 
-const Piece = ({ name, pos }) => {
+const Piece = ({ name, pos, setFromPos }) => {
     const color = name === name.toUpperCase() ? 'w' : 'b';
     const imageName = color + name.toUpperCase();
+    const element = useRef();
+
     let image;
 
     try {
         image = require(`../../assets/pieces/${imageName}.png`);
     } catch (error) {
-        image = require('../../assets/pieces/empty.png'); //an empty fallback image
+        image = require('../../assets/pieces/empty.png');
     }
+
+    const handleDragStart = () => {
+        setFromPos(pos);
+        setTimeout(() => {
+            element.current.style.display = 'none';
+        }, 0);
+    };
+    const handleDragEnd = () => {
+        element.current.style.display = 'block';
+    };
 
     return (
         <img
@@ -19,6 +31,9 @@ const Piece = ({ name, pos }) => {
             src={image}
             alt=""
             draggable={true}
+            ref={element}
+            onDragStart={handleDragStart}
+            onDragEnd={handleDragEnd}
         />
     );
 };
@@ -26,5 +41,6 @@ const Piece = ({ name, pos }) => {
 Piece.prototype = {
     name: PropTypes.string.isRequired,
     pos: PropTypes.string.isRequired,
+    setFromPos: PropTypes.func.isRequired,
 };
 export default Piece;
--- src/pages/Game/index.jsx
+++ src/pages/Game/index.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useRef, useEffect } from 'react';
-//import Chess from 'chess.js';
-import { Chess } from 'chess.js'
+import { Chess } from 'chess.js';
 import { createBoard } from '../../functions';
 import Board from '../../components/board';
 
@@ -9,14 +8,24 @@ const Game = () => {
     const [fen, setFen] = useState(FEN);
     const { current: chess } = useRef(new Chess(fen));
     const [board, setBoard] = useState(createBoard(fen));
-
     useEffect(() => {
         setBoard(createBoard(fen));
     }, [fen]);
 
+    const fromPos = useRef();
+
+    const makeMove = (pos) => {
+        const from = fromPos.current;
+        const to = pos;
+        chess.move({ from, to });
+        setFen(chess.fen());
+    };
+
+    const setFromPos = (pos) => (fromPos.current = pos);
+
     return (
         <div className="game">
-            <Board cells={board} />
+            <Board cells={board} makeMove={makeMove} setFromPos={setFromPos} />
         </div>
     );
 };
